a285fdde9f42439605430d4e9e231013b5aaed34
Gennaro Nolano <nolanogenn@gmail.com>
2022-08-22 17:06:08 +0200

    update

--- materiali/introduzione.R
+++ materiali/introduzione.R
@@ -14,10 +14,10 @@ librerie <- c('tidyverse',
 	      "irr",
 	      "RTextTools",
 	      "e1071",
-	      "rlang"
+	      "rlang",
 	      "hardhat",
 	      "recipes",
-	      "caret",
+	      "caret"
 )
 librerie_nuove <- librerie[!(librerie %in% installed.packages()[,"Package"])]
 if(length(librerie_nuove)>0)
@@ -40,13 +40,12 @@ cat("parte intera di 17/5 =", 17%/%5)
 x <- 5
 x + 6
 
-
 #controllare la classe di una variabile
 class(x)
 
 #stringhe e caratteri 
 frutta <- "mela"
-
+class(frutta)
 
 # numero di caratteri in una stringa
 nchar(frutta)
@@ -61,7 +60,10 @@ paste(m,e,l,a)
 paste(m,e,l,a, sep='')
 
 #sottostringa
+
+frase <- "sono al corso di Sentiment Analysis, mi sta piacendo"
 substr(frutta, 1, 2)
+substr(frase,1,40)
 
 #booleani
 bool1 <- TRUE
@@ -88,14 +90,19 @@ grepl("mela", "una mela al giorno")
 #liste di elementi uguali
 
 stringhe <- character(10)
+stringhe
 logici <- logical(7)
+logici
 numer_interi <- integer(8)
+numer_interi
 
 #vettore vuoto
 x <- c()
+x
 
 #vettore personalizzato
 x <- c("Mariapia", "Gennaro")
+x
 
 #aggiungere elementi
 y <- c(x, "Felice")
@@ -106,21 +113,68 @@ print(z)
 
 #serie di numeri
 serie <- 1:10
-
+serie
 #recuperare elementi dai vettori
 numeri_pari <- seq(from=0, to=100, by=2)
-print(numeri_pari)
 numeri_pari[12]
+
+frasi <- c("una mela al giorno",
+           "una pera alla settimana",
+           "un mirtillo al mese",
+           "una Mela al giorno")
+
+"mela" == "Mela"
+sum(grepl("mela", frasi))
 
 #dataframe
 
 nomi <- c("Mario", "Chiara", "Simona")
 anno_di_nascita <- c(1993, 1995, 1996)
 
+?data.frame
+
 data <- data.frame(
   "persone" = nomi,
   "anno_di_nascita" = anno_di_nascita
 )
+
+nomi <- c("Mariapia", "Gennaro", "Andrea",
+          "Giacomo", "Nicola", "Maria Grazia","Luigi", "Dario")
+paesi_di_nascita <- c("Napoli", "Acerra", "Catania", "Napoli",
+                      "Salerno", "Lecce", "Napoli", "Salerno")
+
+nomi[1]
+paesi_di_nascita[1]
+
+
+dataframe <- data.frame(
+  "names" = nomi,
+  "birth places" =paesi_di_nascita
+)
+
+class(dataframe)
+nrow(dataframe)
+ncol(dataframe)
+
+names(dataframe)
+
+sum(dataframe$birth.places == "Napoli")
+
+
+dataframe
+
+genere_vettore <- c("F", "M", "M", "M", "M", "F", "M", "M")
+dataframe$genere <- genere_vettore
+
+sum(
+  dataframe$birth.places == "Napoli" &
+    dataframe$genere == "M")
+
+dataframe[2,1]
+
+dataframe[3,1:2]
+
+summary(dataframe)
 print("---Dataframe:---")
 data
 print("---Numero di righe---")
@@ -132,9 +186,11 @@ colnames(data)
 print("---Colonna 'persone'--")
 data["persone"]
 
+colnames(dataframe)
+
 #funzione
 
 nuova_funzione <- function(a,b){
-  print(a + b)
+  return(a%%b)
 }
 nuova_funzione(2,3)
